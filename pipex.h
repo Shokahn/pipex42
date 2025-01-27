dd752112d7c29a3f251dae210304bb10f01f6c4b
Stefano Devis <stdevis@mi-c1-p1.s19.be>
2025-01-27 19:23:53 +0100

    starting pipex

---
+++ pipex.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stdevis <stdevis@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/27 17:34:16 by stdevis           #+#    #+#             */
+/*   Updated: 2025/01/27 19:11:01 by stdevis          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include "libft/headers/libft.h"
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+# include <sys/wait.h>
+
+
+#endif
